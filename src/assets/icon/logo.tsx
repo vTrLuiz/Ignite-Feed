d54f9dd20488fd299e4cd795e18b66455f5a9f07
vTrLuiz <contatovluiz@gmail.com>
2024-07-25 17:00:09 -0300

    Fix: Correção do Css / Feat: Add font roboto

---
+++ src/assets/icon/logo.tsx
@@ -0,0 +1,18 @@
+export function Logo () {
+
+    return(
+	<svg width="64.082565" height="60.764606" viewBox="0 0 64.0826 60.7646" fill="none" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+	<desc>
+			Created with Pixso.
+	</desc>
+	<defs/>
+	<g opacity="0.500000">
+		<path id="Vector" d="M64.06 0.41L48.38 55.95C48.3 56.2 47.98 56.28 47.8 56.1L37.3 46.14L45.17 18.31C45.25 18.05 45.01 17.83 44.74 17.9L15.4 25.37L4.96 15.46C4.77 15.28 4.85 14.98 5.12 14.9L63.64 0C63.9 -0.05 64.14 0.18 64.06 0.41Z" fill="#00B37E" fill-opacity="1.000000" fill-rule="nonzero"/>
+	</g>
+	<g opacity="0.500000">
+		<path id="Vector" d="M37.3 46.14L33.26 60.51C33.18 60.76 32.86 60.84 32.68 60.66L0.1 29.78C-0.09 29.6 -0.01 29.3 0.26 29.22L15.4 25.37L37.3 46.14Z" fill="#00B37E" fill-opacity="1.000000" fill-rule="nonzero"/>
+	</g>
+	<path id="Vector" d="M45.17 18.31L37.3 46.14L15.4 25.37L44.74 17.9C45.01 17.83 45.25 18.05 45.17 18.31Z" fill="#00B37E" fill-opacity="1.000000" fill-rule="nonzero"/>
+</svg>
+    )
+}
